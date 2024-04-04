8579895840125d8bf04b71d92eecf6c48207f924
Sara Russo <pandessa@gmail.com>
2024-04-04 15:51:04 +0200

    commit

--- styling/src/components/Header.jsx
+++ styling/src/components/Header.jsx
@@ -1,11 +1,15 @@
-import logo from '../assets/logo.png';
+import logo from "../assets/logo.png";
 
 export default function Header() {
-  return (
-    <header>
-      <img src={logo} alt="A canvas" />
-      <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
-    </header>
-  );
+	return (
+		<header className="flex flex-col items-center mt-8 mb-16">
+			<img
+				className="w-16"
+				src={logo}
+				alt="A canvas"
+			/>
+			<h1 className="text-4xl font-semibold tracking-widest text-center uppercase text-amber-800">ReactArt</h1>
+			<p>A community of artists and art-lovers.</p>
+		</header>
+	);
 }
